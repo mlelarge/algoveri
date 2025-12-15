3b689c6c7dc09610ab1d895b37af9f9fff4f4095
Jiawei-Li20 <jiaweili.g2019@gmail.com>
2025-12-14 22:05:30 -0500

    add bubble sort and insertion sort for verus, add gemini-3 results (solved)

---
+++ algoveri_data/insertion_sort/verus_spec.rs
@@ -0,0 +1,57 @@
+use vstd::prelude::*;
+
+verus! {
+    // Following is the block for necessary definitions
+    // <preamble>
+    spec fn is_sorted(s: Seq<i32>) -> bool {
+        forall|i: int, j: int| 0 <= i < j < s.len() ==> s[i] <= s[j]
+    }
+
+    spec fn is_valid_index_permutation(p: Seq<int>, n: int) -> bool {
+        p.len() == n
+        && (forall|i: int| 0 <= i < n ==> 0 <= #[trigger] p[i] < n)
+        && (forall|i: int, j: int| 0 <= i < j < n ==> #[trigger] p[i] != #[trigger] p[j])
+    }
+
+    spec fn is_permutation(v1: Seq<i32>, v2: Seq<i32>) -> bool {
+        exists|p: Seq<int>| 
+            is_valid_index_permutation(p, v1.len() as int) 
+            && v1.len() == v2.len()
+            && (forall|i: int| 0 <= i < v1.len() ==> v2[i] == v1[#[trigger] p[i]])
+    }
+    // </preamble>
+
+    // Following is the block for potential helper specifications
+    // <helpers>
+
+    // </helpers>
+
+    // Following is the block for proofs of lemmas, or functions that help the implementation or verification of the main specification
+    // <proofs>
+
+    // </proofs>
+
+    // Following is the block for the main specification
+    // <spec>
+    fn insertion_sort(v: &mut Vec<i32>)
+        ensures
+            is_sorted(v@),
+            is_permutation(old(v)@, v@),
+    // </spec>
+    // <code>
+    {
+
+    }
+    // </code>
+
+    #[verifier::external]
+    fn main() {
+        let mut v = vec![5, 1, 4, 2, 8];
+        let ghost original_v = v@;
+        
+        insertion_sort(&mut v);
+        
+        assert(is_sorted(v@));
+        assert(is_permutation(original_v, v@));
+    }
+}
