430d917e11e131ce46f37fb8f79fe49c51229f3f
Jiawei-Li20 <jiaweili.g2019@gmail.com>
2025-12-14 22:39:32 -0500

    add merge sort and quick sort (deterministic version), add gemini-3 result (cannot verify)

--- test/test_runner_jiawei.py
+++ test/test_runner_jiawei.py
@@ -9,7 +9,7 @@ def test_runner_verus():
     llm_provider = GeminiProvider()
     cfg_path = Path(__file__).resolve().parent / "config_jiawei_test.yaml"
     runner = Runner(llm_provider=llm_provider, language="verus", cfg_path=str(cfg_path), results_root="test_results/verus")
-    res = runner.run_problem(problem_dir="/scratch/gpfs/ARORA/haoyu/jiawei/algoveri/algoveri_data/merge_sort", max_rounds=15, model="gemini-3-pro-preview", debug=True)
+    res = runner.run_problem(problem_dir="/scratch/gpfs/ARORA/haoyu/jiawei/algoveri/algoveri_data/quick_sort", max_rounds=15, model="gemini-3-pro-preview", debug=True)
     print(res)
 
 if __name__ == "__main__":
